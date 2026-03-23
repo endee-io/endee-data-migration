0ce6a7e6e88a0120d322cfd7bb3251d6442835d3
Omnish_Kumar <omnish@endee.io>
2026-03-23 15:25:05 +0530

    docker compose file added

--- cmd.sh
+++ cmd.sh
@@ -1,7 +1,8 @@
 docker build -t vector-migration:latest .
-docker run \
-  --rm \
-  --network vector-net \
-  --env-file .env.dev \
-  -v $(pwd)/data:/app/data \
-  vector-migration:latest
+docker compose up --build
+# docker run \
+#   --rm \
+#   --network vector-net \
+#   --env-file .env.dev \
+#   -v $(pwd)/data:/app/data \
+#   vector-migration:latest
